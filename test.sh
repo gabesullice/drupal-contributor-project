03d12cded21d5dfe1a9f37bb3c40224ce0a2716d
Gabriel SULLICE <gabriel@sullice.com>
2020-02-14 15:48:35 -0700

    Remove the installer plugin.

--- test.sh
+++ test.sh
@@ -4,19 +4,13 @@ set -ev
 
 SCRATCH_DIR=scratch
 
-rm -rf "$SCRATCH_DIR"/vendor/drupal/*
-
-mv "$SCRATCH_DIR"/vendor ./
-
 rm -rf "$SCRATCH_DIR"
 
 mkdir "$SCRATCH_DIR"
 
-mv vendor "$SCRATCH_DIR"/
+cp ./composer.json "$SCRATCH_DIR"/
 
 pushd "$SCRATCH_DIR"/
-
-cp ../drupal-contrib-project/composer.json ./
 
 composer install -v
 
